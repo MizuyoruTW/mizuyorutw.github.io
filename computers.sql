26ccc64dcca6116cca7f6fc6c018479ba019df89
MizuyoruTW <mizuyoru.tw@gmail.com>
2021-11-20 22:23:10 +0800

    fixed wrong sql output

--- computers.sql
+++ computers.sql
@@ -14,11 +14,11 @@ SELECT OS FROM computers;
     |Windows       |1 |*/
 
 SELECT * FROM spec WHERE id = 0;
-/*  |CPU            |GPU                               |RAM|
-    |---------------|----------------------------------|---|
-    |Intel i7-6700HQ|Intel HD graphics, Nvidia GTX 960M|16G|*/
+/*  |id|CPU            |GPU                               |RAM|
+    |--|---------------|----------------------------------|---|
+    |0 |Intel i7-6700HQ|Intel HD graphics, Nvidia GTX 960M|16G|*/
 
 SELECT * FROM spec WHERE id = 1;
-/*  |CPU          |GPU            |RAM|
-    |-------------|---------------|---|
-    |Intel i5-8400|Nvidia GTX 1050|8G |*/
+/*  |id|CPU          |GPU            |RAM|
+    |--|-------------|---------------|---|
+    |1 |Intel i5-8400|Nvidia GTX 1050|8G |*/
